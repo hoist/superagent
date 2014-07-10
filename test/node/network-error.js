3adecd2b2a4342cbf3fb099c5b5294a1c3a4469d
Owen Evans <owen@bgeek.net>
2014-07-10 15:36:30 +1200

    cope with nock not allowing connection

--- test/node/network-error.js
+++ test/node/network-error.js
@@ -1,36 +1,63 @@
-
-var request = require('../..')
-  , express = require('express')
-  , assert = require('assert')
-  , net = require('net');
+var request = require('../..'),
+  express = require('express'),
+  assert = require('assert'),
+  net = require('net'),
+  nock = require('nock');
 
 function getFreePort(fn) {
   var server = net.createServer();
-  server.listen(0, function(){
+  server.listen(0, function () {
     var port = server.address().port;
-    server.close(function(){
+    server.close(function () {
       fn(port);
     });
   });
 };
 
-describe('with network error', function(){
-  before(function(done){
+describe('with network error', function () {
+  before(function (done) {
     var self = this;
     // connecting to a free port
     // will trigger a connection refused
-    getFreePort(function(port){
+    getFreePort(function (port) {
       self.port = port;
       done();
     });
   });
 
-  it('should error', function(done) {
+  it('should error', function (done) {
     request
-    .get('http://localhost:' + this.port + '/')
-    .end(function(err, res){
-      assert(err, 'expected an error');
-      done();
-    });
+      .get('http://localhost:' + this.port + '/')
+      .query({
+        key: 'val'
+      })
+      .end(function (err, res) {
+        assert(err, 'expected an error');
+        done();
+      });
+  });
+});
+describe('with nock connection not allowed and query', function () {
+  before(function () {
+    var self = this;
+    //mock out the client **from nock.js code https://github.com/pgte/nock
+    nock.disableNetConnect();
+  });
+
+  it('should error', function (done) {
+    request
+      .get('http://localhost:' + this.port + '/')
+      .query({
+        key: 'val'
+      })
+      .end(function (err, res) {
+        
+        assert(err, 'expected an error');
+        err.name.should.eql('NetConnectNotAllowedError');
+        done();
+      });
+  });
+  after(function () {
+    nock.enableNetConnect();
   });
 });
